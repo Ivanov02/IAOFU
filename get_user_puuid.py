7ef4e7034bdf25942a0525459dd8a35443d16b9b
IvanovF <ivanovflorin2@yahoo.com>
2024-10-13 23:04:00 +0300

    better usage of the constants_and_parameters files

--- get_user_puuid.py
+++ get_user_puuid.py
@@ -1,13 +1,13 @@
 import requests
+import constants_and_parameters
 
 
-def get_user_puuid(api_url_a, api_key_a, user_name_a, user_tag_line_a):
-    api_url = api_url_a
+def get_user_puuid(api_key_a, user_name_a, user_tag_line_a):
     api_key = api_key_a
     user_name = user_name_a
     user_tag_line = user_tag_line_a
 
-    api_request_link = f"{api_url}/{user_name}/{user_tag_line}?api_key={api_key}"
+    api_request_link = f"{constants_and_parameters.api_url_get_puuid}/{user_name}/{user_tag_line}?api_key={api_key}"
 
     response = requests.get(api_request_link)
     output = response.json()

f75a1221c14163cdce1cc5f41a68884c64688104
IvanovF <ivanovflorin2@yahoo.com>
2024-10-20 23:17:18 +0300

    managed to get user lane, user items

--- get_user_matches_ids.py
+++ get_user_matches_ids.py
@@ -1,15 +1,15 @@
 import requests
-
 import constants_and_parameters
 from get_user_puuid import get_user_puuid
 
 
-def get_user_mathces_ids(api_key_a, start_a, count_a, api_url_puuid_a, user_name_a,
-                         user_tag_line_a):
-    user_puuid = get_user_puuid(api_url_puuid_a, api_key_a, user_name_a, user_tag_line_a)
+def get_user_mathces_ids(start_a, count_a):
+    user_puuid = get_user_puuid(constants_and_parameters.api_key, constants_and_parameters.user_name,
+                                constants_and_parameters.user_tag_name)
     match_ids_url = f'{constants_and_parameters.api_url_get_matches_ids}/{user_puuid}/ids'
-    params = {'api_key': api_key_a, 'start': start_a, 'count': count_a}
+    params = {'api_key': constants_and_parameters.api_key, 'start': start_a, 'count': count_a}
     response = requests.get(match_ids_url, params=params)
     match_ids = response.json()
 
     return match_ids
+
